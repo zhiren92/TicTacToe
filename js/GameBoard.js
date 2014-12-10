925c7877f16a2a4a6deb42aa6fcb79d89031eb59
Zillah Chen <zillah_c@hotmail.com>
2014-12-09 20:38:14 -0800

    added exchanging X and O and prevent replacing values

--- js/GameBoard.js
+++ js/GameBoard.js
@@ -8,7 +8,6 @@ GameBoardFunc.$inject=['$firebase']
 
 function GameBoardFunc($firebase) {
 
-
 	var GameBoard= function(){
 
 
@@ -16,42 +15,59 @@ function GameBoardFunc($firebase) {
 		
 	
 		var InitialValues = [
-			{name:"1", XorO:""},
-			{name:"2", XorO:""},
-			{name:"3", XorO:""},
-			{name:"4", XorO:""},
-			{name:"5", XorO:""},
-			{name:"6", XorO:""},
-			{name:"7", XorO:""},
-			{name:"8", XorO:""},
-			{name:"9", XorO:""}
+			{value:"", XorO:""},
+			{value:"", XorO:""},
+			{value:"", XorO:""},
+			{value:"", XorO:""},
+			{value:"", XorO:""},
+			{value:"", XorO:""},
+			{value:"", XorO:""},
+			{value:"", XorO:""},
+			{value:"", XorO:""}
 		];
 
 	var ref = new Firebase("https://tttproject.firebaseIO.com/GameBoard");
 	this.GameBoardSquares = $firebase(ref).$asArray();
 
-	// for (var i = 0; i < InitialValues.length; i++) {
-	// 	this.GameBoardSquares.$add(InitialValues[i]);
-	// };
+	for (var i = 0; i < InitialValues.length; i++) {
+		this.GameBoardSquares.$add(InitialValues[i]);
+	};
 
 
-
+	var counter = 0;
 
 
 
 	this.PutOnBoard = function($index){
-		this.GameBoardSquares[$index].XorO="X";
-		this.GameBoardSquares.$save($index);
+
+		if(this.GameBoardSquares[$index].XorO !== ""){
+			return false;	
+		}
+		
+
+		counter++
+		if(counter%2!==0){
+			this.GameBoardSquares[$index].XorO="X";
+			this.GameBoardSquares.$save($index);
+		} else {
+			this.GameBoardSquares[$index].XorO="O";
+			this.GameBoardSquares.$save($index);
 	}
 
 
-	this.ClearBoard = function($index){
+	}
 
-		alert("work")
-		// this.GameBoardSquares[$index].XorO="";
+
+	this.ClearBoard = function(){
+
 		
-		this.GameBoardSquares[$index].XorO="O";
-		this.GameBoardSquares.$save();
+	for (var i = 0; i < this.GameBoardSquares.length; i++) {
+	this.GameBoardSquares.$remove(this.GameBoardSquares[i]);
+	};
+
+	for (var i = 0; i < InitialValues.length; i++) {
+	this.GameBoardSquares.$add(InitialValues[i]);
+	};
 
 	}
 
